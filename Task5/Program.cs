42f9b237a891c4d72ec11c37e11579495543d8d1
Anton <korjant09996@gmail.com>
2022-09-12 17:24:54 +0400

    ZADanie 5 complite

---
+++ Task5/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход приннимает два числа и выдает, какое число больше, а какое меньше
+Console.WriteLine("Данная программа , сравнит предложеные вами числа указав на наибольшее. Введите 1 число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2 число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b) {
+    Console.Write($"a={a};b={b} -> max = {a} ");
+}
+else if (b > a) {
+  Console.Write(($"a={a};b={b} -> max = {b} "));
+}
+else {
+    Console.Write("числa равны");
+}
