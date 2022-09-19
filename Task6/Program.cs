f734806590763bf303211a4f71a44e5bedadd19f
Anton <korjant09996@gmail.com>
2022-09-19 13:29:24 +0400

    ZADanie 6  complite

---
+++ Task6/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих
+Console.WriteLine("Данная программа , сравнит предложеные вами числа указав на наибольшее. Введите 1 число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2 число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 3 число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (b > max) 
+    max = b;
+if (c > max) 
+    max = c;
+else {
+    Console.Write("числa равны ");
+}
+Console.Write($"a={a};b={b};c={c} -> max = {max} ");
